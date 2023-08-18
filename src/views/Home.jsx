67c9bee6cc92335c9447aa7b90c6f6ff374a64e4
Paulinavegac <paulina.vegac@gmail.com>
2023-08-18 12:39:13 -0400

    ADD: firebase config for firebase storage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,6 @@
 import './Home.css'
-import logo from '../assets/img/logo.png'
 import Nav from '../components/Nav'
+//import { storage } from './firebase';
 function Home() {
 
   return (
@@ -10,7 +10,11 @@ function Home() {
       </section>
 
       <figure className='container-logo'>
-        <img src={logo} className='logo-home' alt='JS Motos logo' />
+        {/* <img src={logo} className='logo-home' alt='JS Motos logo' /> */}
+        <img className='logo-home' src=
+        "https://firebasestorage.googleapis.com/v0/b/jsmotos-e4995.appspot.com/o/webpage%2FlogoJsMotos.png?alt=media&token=83956378-6ff7-47cf-9866-d92cde38e2e2"
+         alt='logoJsMotos' />
+
       </figure>
 
       <p className="read-the-docs">
