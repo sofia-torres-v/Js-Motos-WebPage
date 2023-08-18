2c0501ec32d83a682a27fdf969b5c5fdb780b552
andrenavas <andreynanavas@gmail.com>
2023-08-18 16:13:59 -0500

    ADD: burger menu

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,21 +4,13 @@ import Nav from '../components/Nav'
 function Home() {
 
   return (
-    <div className='home-view'>
-      <section className='container-nav-home'>
+    <div className='home-view' id='outer-container'>
+      <section className='container-nav-home' id='page-wrap'>
         <Nav />
       </section>
 
-      <figure className='container-logo'>
-        {/* <img src={logo} className='logo-home' alt='JS Motos logo' /> */}
-        <img className='logo-home' src=
-        "https://firebasestorage.googleapis.com/v0/b/jsmotos-e4995.appspot.com/o/webpage%2FlogoJsMotos.png?alt=media&token=83956378-6ff7-47cf-9866-d92cde38e2e2"
-         alt='logoJsMotos' />
-
-      </figure>
-
       <p className="read-the-docs">
-        Hola Andre!!!
+        ¡Buenas, buenas! Equipo dinamita 🧨
       </p>
     </div>
   )
