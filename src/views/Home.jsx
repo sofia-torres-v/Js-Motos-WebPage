aeb9b5779bc7aa8ec983236689fc919f067fa6aa
andrenavas <andreynanavas@gmail.com>
2023-08-18 16:41:38 -0500

    ADD: motobike image in home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,10 +8,17 @@ function Home() {
       <section className='container-nav-home' id='page-wrap'>
         <Nav />
       </section>
-
-      <p className="read-the-docs">
-        ¡Buenas, buenas! Equipo dinamita 🧨
-      </p>
+      <main className='container-main-home'>
+        <span className='home-title'>
+          ¡TODOS los accesorios para tu moto!
+        </span>
+        <figure className='container-img-motobike'>
+          {/* <img src={logo} className='logo-home' alt='JS Motos logo' /> */}
+          <img className='img-motobike'
+            src='https://firebasestorage.googleapis.com/v0/b/jsmotos-e4995.appspot.com/o/webpage%2FyellowMotobike.png?alt=media&token=8e681834-36e5-470c-b74c-562906b97f33'
+            alt='image motobike' />
+        </figure>
+      </main>
     </div>
   )
 }
