91be80b2631369338071e13027802f275c3dbc4d
andrenavas <andreynanavas@gmail.com>
2023-11-29 16:00:25 -0500

    UPDATE: new image in about view

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -12,33 +12,36 @@ const About = () => {
             </header>
             <main>
                 <section className="container-main-about">
-                    <div className="box-image">
-                        <img
-                            className="img-motorcicle"
-                            src={motoImage}
-                            alt="image motorcicle"
-                        />
-                    </div>
-                    <div className="container-text">
-                        <h1 className="title-about">Conócenos</h1>
-                        <div className="text-about">
-                        <p >
-                            Somos una empresa Peruana dedicada a la fabricación
-                            y venta de empaquetaduras para motos.Tenemos
-                            nuestros inicios en el año 2017, nace como un un
-                            pequeño emprendimiento, el cual con mucho esfuerzo y
-                            dedidación, se ha convertido en JS MOTOS import, el cual es nuestra pasión.
-                            Atender a nuestros clientes con excelente servicio y productos fabricados con materiales
-                            de alta calidad es nuestra misión.
-                
-                        </p>
+                    <div className="container-image-text">
+                        <div className="box-image">
+                            <img
+                                className="img-motorcicle"
+                                src={motoImage}
+                                alt="image motorcicle"
+                            />
+                        </div>
+                        <div className="container-text">
+                            <h1 className="title-about">CONÓCENOS</h1>
+                            <div className="title-line"></div>
+                            <div className="text-about">
+                                <p >
+                                    Somos una empresa Peruana dedicada a la fabricación
+                                    y venta de empaquetaduras para motos.Tenemos
+                                    nuestros inicios en el año 2017, nace como un un
+                                    pequeño emprendimiento, el cual con mucho esfuerzo y
+                                    dedidación, se ha convertido en JS MOTOS import, el cual es nuestra pasión.
+                                    Atender a nuestros clientes con excelente servicio y productos fabricados con materiales
+                                    de alta calidad es nuestra misión.
+
+                                </p>
+
+                            </div>
 
                         </div>
-                    
                     </div>
                     <div className="icons-info values">
                         <h1 className="title-values">NUESTROS VALORES</h1>
-                        <ValuesGroup/>
+                        <ValuesGroup />
                     </div>
                 </section>
             </main>
