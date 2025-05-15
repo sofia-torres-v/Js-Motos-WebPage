42cae5deca4111a1b2123891823cfb77c10fe3b7
Sofía Torres <sofy.torresv@gmail.com>
2025-05-14 22:24:51 -0500

    Add: WhatsApp icon

--- src/firebaseConfig/imagesUrls.js
+++ src/firebaseConfig/imagesUrls.js
@@ -1,4 +1,4 @@
 export const images = {
-
+   whatsApp: 'https://firebasestorage.googleapis.com/v0/b/jsmotos-e4995.appspot.com/o/webpage%2FWhatsapp-icon-mobile.png?alt=media&token=79a5e521-f26b-4b6f-b64f-f3ff6943be24',
     bannerHome:'https://firebasestorage.googleapis.com/v0/b/jsmotos-e4995.appspot.com/o/webpage%2FbannerHome.jpg?alt=media&token=e9feb13f-fad8-4aaa-bb39-609abdde3186'
 };
